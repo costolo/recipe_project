2e7d4393711f284d06dd45f972109abf7d96cb88
Harry Park <imharrypark@gmail.com>
2015-04-10 17:30:38 -0400

    create method to validate username

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -17,3 +17,16 @@ end
 def current_user
   User.find(session[:user_id]) if session[:user_id]
 end
+
+def validate_account(name, password)
+  name_valid?(name) && password_valid?(password)
+end
+
+def name_valid?(name)
+  valid_chars = ("A".."Z").to_a + ("a".."z").to_a + ["-","_"]
+  name.length.between?(5,16) && name.split('').all? {|i| valid_chars.include?(i)}
+end
+
+def password_valid?(password)
+
+end
