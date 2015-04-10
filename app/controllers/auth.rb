06d8b6b6599404b62cea687d02ab38a0b849a57d
Harry Park <imharrypark@gmail.com>
2015-04-10 15:37:03 -0400

    authenticate and session user upon sign in

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,7 +3,19 @@ get '/signin' do
 end
 
 post '/signin' do
+  unconfirmed_user = User.find_by(name: params[:name])
 
+  if unconfirmed_user
+    user = unconfirmed_user.authenticate(params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect '/'
+    else
+      redirect '/signin'
+    end
+  else
+    redirect '/signin'
+  end
 end
 
 get '/signup' do
